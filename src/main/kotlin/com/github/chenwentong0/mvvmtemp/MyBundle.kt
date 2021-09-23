711153dfec23f8a28fc33ebf114bcd5a12218d5a
GitHub Action <action@github.com>
2021-09-23 06:22:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/chenwentong0/mvvmtemp/MyBundle.kt
+++ src/main/kotlin/com/github/chenwentong0/mvvmtemp/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.chenwentong0.mvvmtemp
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/chenwentong0/mvvmtemp/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/chenwentong0/mvvmtemp/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.chenwentong0.mvvmtemp.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.chenwentong0.mvvmtemp.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
